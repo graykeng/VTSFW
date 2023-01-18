3db4f67cee70511642794701b0f04ac05159f34b
graykeng <graykeng7@gmail.com>
2023-01-17 17:44:01 -0800

    changed to aws

--- src/ApiCaller.js
+++ src/ApiCaller.js
@@ -1,32 +1,34 @@
 import Axios from "axios";
 
+const url = "https://ec2-52-13-3-113.us-west-2.compute.amazonaws.com:8080"
+
 export const getEventPic = () => {
-    return Axios.get("https://vtsf-backend.herokuapp.com/api/EventPic")
+    return Axios.get(url + "/api/EventPic")
         .then((response) => response.data);
 };
 
 export const getCommiteeInfo = () => {
-    return Axios.get("https://vtsf-backend.herokuapp.com/api/CommiteeInfo")
+    return Axios.get(url + "/api/CommiteeInfo")
         .then((response) => response.data);
 };
 
 export const getSponsorsInfo = () => {
-    return Axios.get("https://vtsf-backend.herokuapp.com/api/SponsorsInfo")
+    return Axios.get(url + "/api/SponsorsInfo")
         .then((response) => response.data);
 };
 
 export const getLogoPic = () => {
-    return Axios.get("https://vtsf-backend.herokuapp.com/api/LogoPic")
+    return Axios.get(url + "/api/LogoPic")
         .then((response) => response.data);
 };
 
 export const getBlogPostGrid = () => {
-    return Axios.get("https://vtsf-backend.herokuapp.com/api/BlogPostGrid")
+    return Axios.get(url + "/api/BlogPostGrid")
         .then((response) => response.data);
 };
 
 export const getPost = (id) => {
-    return Axios.get(`https://vtsf-backend.herokuapp.com/api/BlogPostDetail/${id}`)
+    return Axios.get(url + `/api/BlogPostDetail/${id}`)
     .then((response) => response.data);
 };
 
