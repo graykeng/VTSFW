e39f0ee58b323990999bb14417f33f1a9346e753
graykeng <graykeng7@gmail.com>
2023-01-19 10:14:16 -0800

    c

--- server/index.js
+++ server/index.js
@@ -84,6 +84,6 @@ app.get("/api/BlogPostDetail/:PostID", (req, res) => {
   })
 })
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 3001, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
