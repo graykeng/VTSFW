81418f686773a75b6e999398cd6a24dbfa4efb72
graykeng <graykeng7@gmail.com>
2023-01-19 10:24:14 -0800

    final

--- src/ApiCaller.js
+++ src/ApiCaller.js
@@ -1,34 +1,33 @@
 import Axios from "axios";
 
-const url = "http://ec2-52-13-3-113.us-west-2.compute.amazonaws.com:8080"
 
 export const getEventPic = () => {
-    return Axios.get(url + "/api/EventPic")
+    return Axios.get("http://ec2-52-13-3-113.us-west-2.compute.amazonaws.com:8080/api/EventPic")
         .then((response) => response.data);
 };
 
 export const getCommiteeInfo = () => {
-    return Axios.get(url + "/api/CommiteeInfo")
+    return Axios.get("http://ec2-52-13-3-113.us-west-2.compute.amazonaws.com:8080/api/CommiteeInfo")
         .then((response) => response.data);
 };
 
 export const getSponsorsInfo = () => {
-    return Axios.get(url + "/api/SponsorsInfo")
+    return Axios.get("http://ec2-52-13-3-113.us-west-2.compute.amazonaws.com:8080/api/SponsorsInfo")
         .then((response) => response.data);
 };
 
 export const getLogoPic = () => {
-    return Axios.get(url + "/api/LogoPic")
+    return Axios.get("http://ec2-52-13-3-113.us-west-2.compute.amazonaws.com:8080/api/LogoPic")
         .then((response) => response.data);
 };
 
 export const getBlogPostGrid = () => {
-    return Axios.get(url + "/api/BlogPostGrid")
+    return Axios.get("http://ec2-52-13-3-113.us-west-2.compute.amazonaws.com:8080/api/BlogPostGrid")
         .then((response) => response.data);
 };
 
 export const getPost = (id) => {
-    return Axios.get(url + `/api/BlogPostDetail/${id}`)
+    return Axios.get(`http://ec2-52-13-3-113.us-west-2.compute.amazonaws.com:8080/api/BlogPostDetail/${id}`)
     .then((response) => response.data);
 };
 
