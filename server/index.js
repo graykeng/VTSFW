63b5bf676573ead9a31390158cf4a2ffb8487052
graykeng <graykeng7@gmail.com>
2023-01-20 12:59:18 -0800

    final fix

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const PORT = 3001;
+const PORT = 3000;
 
 const db = require("./config/db");
 
@@ -84,6 +84,6 @@ app.get("/api/BlogPostDetail/:PostID", (req, res) => {
   })
 })
 
-app.listen(process.env.PORT || 3001, function(){
+app.listen(PORT, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
