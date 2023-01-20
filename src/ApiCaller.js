5288c850cbfae07a765b40e67b3779d16f24bc28
graykeng <graykeng7@gmail.com>
2023-01-19 18:41:07 -0800

    final

--- src/ApiCaller.js
+++ src/ApiCaller.js
@@ -2,32 +2,32 @@ import Axios from "axios";
 
 
 export const getEventPic = () => {
-    return Axios.get("https://ec2-52-13-3-113.us-west-2.compute.amazonaws.com:8080/api/EventPic")
+    return Axios.get("http://ec2-52-13-3-113.us-west-2.compute.amazonaws.com:8080/api/EventPic")
         .then((response) => response.data);
 };
 
 export const getCommiteeInfo = () => {
-    return Axios.get("https://ec2-52-13-3-113.us-west-2.compute.amazonaws.com:8080/api/CommiteeInfo")
+    return Axios.get("http://ec2-52-13-3-113.us-west-2.compute.amazonaws.com:8080/api/CommiteeInfo")
         .then((response) => response.data);
 };
 
 export const getSponsorsInfo = () => {
-    return Axios.get("https://ec2-52-13-3-113.us-west-2.compute.amazonaws.com:8080/api/SponsorsInfo")
+    return Axios.get("http://ec2-52-13-3-113.us-west-2.compute.amazonaws.com:8080/api/SponsorsInfo")
         .then((response) => response.data);
 };
 
 export const getLogoPic = () => {
-    return Axios.get("https://ec2-52-13-3-113.us-west-2.compute.amazonaws.com:8080/api/LogoPic")
+    return Axios.get("http://ec2-52-13-3-113.us-west-2.compute.amazonaws.com:8080/api/LogoPic")
         .then((response) => response.data);
 };
 
 export const getBlogPostGrid = () => {
-    return Axios.get("https://ec2-52-13-3-113.us-west-2.compute.amazonaws.com:8080/api/BlogPostGrid")
+    return Axios.get("http://ec2-52-13-3-113.us-west-2.compute.amazonaws.com:8080/api/BlogPostGrid")
         .then((response) => response.data);
 };
 
 export const getPost = (id) => {
-    return Axios.get(`https://ec2-52-13-3-113.us-west-2.compute.amazonaws.com:8080/api/BlogPostDetail/${id}`)
+    return Axios.get(`http://ec2-52-13-3-113.us-west-2.compute.amazonaws.com:8080/api/BlogPostDetail/${id}`)
     .then((response) => response.data);
 };
 
