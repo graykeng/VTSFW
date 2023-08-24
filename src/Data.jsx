5ff99faccc6192f45c9c4dfd65c5aa9550050621
graykeng <graykeng7@gmail.com>
2023-08-24 08:50:34 -0700

    edited workshop name

--- src/Data.jsx
+++ src/Data.jsx
@@ -1,5 +1,5 @@
 // GetTicket Page
-export const workshopOption = ["Select workshop", "W1: Harmonizing Cultural and Creative Innovation: A Music Journey with Taiwanese Values", "W2: Unleashing Entrepreneurial Potential with an Innovative Mindset", "W3: Navigating through the Tech-Driven Era: Follow the Technology Trend with Digital Mindset"];
+export const workshopOption = ["Select workshop", "W1: Harmonizing Cultural and Creative Innovation: A Music Journey with Taiwanese Values", "W2: Unleashing Entrepreneurial Potential with an Innovative Mindset", "W3: Navigating through the Tech-Driven Era by Following the New Waves in Technology"];
 // export const dietaryOption = ["Select dietary option", "None", "Vegetarian", "Gluten allergy"];
 export const occupationOption = ["Select occupation", "University student", "Graduate student", "Part-time employee", "Full-time employee", "Other"];
 export const heardOption = ["Select", "Social Media (FB&IG)", "Recommended by friend", "University student club", "Website", "Dcard", "Other"];
