9b00ec98470d0e340b293ce2733ff3c11fa70fa5
graykeng <graykeng7@gmail.com>
2023-08-24 20:19:18 -0700

    changed e transfer password

--- src/pages/GetTicket.jsx
+++ src/pages/GetTicket.jsx
@@ -211,7 +211,7 @@ const GetTicket = () => {
           <div className='ReminderSection'>
             <h1 className='ReminderH1'>Last step...</h1>
             <p className='EtransferP'>To pay your ticket, please E-transfer $20 to vtsf2022@gmail.com with your name in the comment. Once we confirmed your transaction, you will receive a confirmation email with an QR code in the next five business days.</p>
-            <p className='EtransferP Red'>E-Transfer Security Question's Answer: VTSF</p>
+            <p className='EtransferP Red'>E-Transfer Security Question's Answer: VTSF2023</p>
             {showCopy && (<button className="ReminderBt copy" data-clipboard-text="vtsf2022@gmail.com" onClick={handleRedeem}>Copy Email</button>)}
             {showBack && (<button className='ReminderBt' onClick={(e) => {window.location.href='/';}}>Back to home page</button>)}
           </div>
