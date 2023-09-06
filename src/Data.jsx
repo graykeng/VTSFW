f3b1dc9a01c4f983f2b602092852363585a57d84
graykeng <graykeng7@gmail.com>
2023-09-05 19:31:25 -0700

    added an option for heardOption

--- src/Data.jsx
+++ src/Data.jsx
@@ -2,7 +2,7 @@
 export const workshopOption = ["Select workshop", "W1: Harmonizing Cultural and Creative Innovation: A Music Journey with Taiwanese Values", "W2: Unleashing Entrepreneurial Potential with an Innovative Mindset", "W3: Navigating through the Tech-Driven Era by Following the New Waves in Technology"];
 // export const dietaryOption = ["Select dietary option", "None", "Vegetarian", "Gluten allergy"];
 export const occupationOption = ["Select occupation", "University student", "Graduate student", "Part-time employee", "Full-time employee", "Other"];
-export const heardOption = ["Select", "Social Media (FB&IG)", "Recommended by friend", "University student club", "Website", "Dcard", "Other"];
+export const heardOption = ["Select", "Social Media (FB&IG)", "Recommended by friend", "University student club", "Website", "Dcard", "School and/or Public Events", "Other"];
 export const trueOrFalse = ["Select", "Yes", "No"];
 
 
